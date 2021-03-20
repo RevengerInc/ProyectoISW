62616bdc0d507d33924d9f9dae94f85fd6ef9267
César <Csar@192.168.1.5>
2021-03-20 01:25:09 -0600

    Pantallas ListaProductos y DetalleProducto. ListaProducto cuenta con un DataTable con diversos artículos que tienen un link que al presionarse los lleva a DetalleProducto donde se despliega el nombre del producto como prueba de que se conserva el producto seleccionado.

--- src/java/Model/Producto.java
+++ src/java/Model/Producto.java
@@ -10,5 +10,92 @@ package Model;
  * @author César
  */
 public class Producto {
+
+    private String id;
+
+    private String nombre;
+
+    private String descripcion;
+
+    private String imagen;
+
+    private double precio;
+
+    private String categoria;
+
+    private int cantidad;
+
+    public Producto(String id, String nombre, String descripcion, String imagen, double precio, String categoria, int cantidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+        this.precio = precio;
+        this.categoria = categoria;
+        this.cantidad = cantidad;
+    }
+
+    public Producto() {
+    }
+    
+    
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    
     
 }
